a2029ef94b9512eaab147a9c52ac6114c31a42fb
onumaeleanyagift <onumaeleanyagift@gmail.com>
2025-07-10 15:23:20 +0100

    feat: add cup selection logic with dynamic big cup fill and water percentage update

--- main.js
+++ main.js
@@ -26,3 +26,28 @@ function selectCups(cupIndex) {
   });
   addBigCup();
 }
+
+function addBigCup() {
+  const totalCups = allSmallCups.length;
+  const fullCups = document.querySelectorAll(".cup.small.full").length;
+  const percent = (fullCups / totalCups) * 100;
+  const liter = 2 - (250 * fullCups) / 1000;
+
+  fill.style.height = `${percent}%`;
+
+  if (fullCups === 0) {
+    percentage.style.visibility = "hidden";
+    liters.innerText = "2L";
+  } else {
+    liters.innerText = `${liter}L`;
+    fill.style.visibility = "visible";
+    percentage.innerText = `${percent}%`;
+    remained.style.visibility = "visible";
+  }
+
+  if (fullCups === totalCups) {
+    remained.style.visibility = "hidden";
+    remained.style.height = 0;
+  } else {
+  }
+}
